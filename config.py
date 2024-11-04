e28a514403c195c2e1effc7e49e61bc0dbafef42
docluis <spam@luisgeissler.de>
2024-11-04 17:12:32 +0100

    added config

---
+++ config.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+class Config:
+    def __init__(self):
+        load_dotenv()
+
+        # Selenium
+        self.chromedriver_path = "/usr/bin/chromedriver"
+        self.service = ChromeService(executable_path=self.chromedriver_path)
+        self.options = ChromeOptions()
+        self.options.add_argument("--lang=en")
+        if self.args.headless:
+            self.options.add_argument("--headless=new")
+        self.driver = webdriver.Chrome(service=self.service, options=self.options)
+
+        # LLM
+        self.model = ChatOpenAI(model="gpt-4o-mini", temperature=0.2)
---
+++ demo.py
@@ -0,0 +1,6 @@
+
+
+
+
+
+prompt = "Buy a bike light on saucedemo.com. Your login credentials are: standard_user, secret_sauce."
